d88a2d095897ad1048c4fd6cce39b27590d743fd
beepscore <support@beepscore.com>
2016-07-22 22:34:43 -0700

    Add difflib_unified_diff

--- diffie/diffie.py
+++ diffie/diffie.py
@@ -44,6 +44,24 @@ if __name__ == '__main__':
         result = list(difflib.ndiff(lines_a, lines_b))
         return result
 
+    def difflib_unified_diff(filename_a, filename_b):
+        """
+        returns differences between two files using difflib_unified_diff
+        https://docs.python.org/3/library/difflib.html
+        """
+
+        lines_a = []
+        lines_b = []
+
+        with open(filename_a) as file_a:
+            lines_a = file_a.readlines()
+
+        with open(filename_b) as file_b:
+            lines_b = file_b.readlines()
+
+        result = list(difflib.unified_diff(lines_a, lines_b))
+        return result
+
 print('differ_compare')
 pprint.pprint(differ_compare('data/a.txt', 'data/b.txt'))
 
@@ -51,3 +69,6 @@ print()
 print('difflib_ndiff')
 pprint.pprint(difflib_ndiff('data/a.txt', 'data/b.txt'))
 
+print()
+print('difflib_unified_diff')
+pprint.pprint(difflib_unified_diff('data/a.txt', 'data/b.txt'))
