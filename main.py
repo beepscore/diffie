8a7303043eed02d0a9347853e3822b4dc56c6824
beepscore <support@beepscore.com>
2016-07-26 01:06:51 -0700

    In main.py comment out print difflib_ndiff. Add print pieces.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pprint
 # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 #if __name__ == '__main__':
 
-print('difflib_ndiff')
-pprint.pprint(difflib_ndiff('data/a.txt', 'data/b.txt'))
+#print('difflib_ndiff')
+#pprint.pprint(difflib_ndiff('data/a.txt', 'data/b.txt'))
 
+print('pieces')
+pprint.pprint(pieces('data/a.txt', 'data/b.txt'))
