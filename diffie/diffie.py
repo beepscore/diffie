45e9c4e3c66c3e8dfdadb7d9a6bf4fbc8dc51d2e
beepscore <support@beepscore.com>
2016-07-25 23:06:36 -0700

    Delete difflib_unified_diff and sequence_matcher_opcodes

--- diffie/diffie.py
+++ diffie/diffie.py
@@ -20,45 +20,6 @@ def difflib_ndiff(filename_a, filename_b):
     results = list(difflib.ndiff(lines_a, lines_b))
     return results
 
-def difflib_unified_diff(filename_a, filename_b):
-    """
-    returns differences between two files using difflib_unified_diff
-    https://docs.python.org/3/library/difflib.html
-    """
-
-    lines_a = []
-    lines_b = []
-
-    with open(filename_a) as file_a:
-        lines_a = file_a.readlines()
-
-    with open(filename_b) as file_b:
-        lines_b = file_b.readlines()
-
-    results = list(difflib.unified_diff(lines_a, lines_b))
-    return results
-
-def sequence_matcher_opcodes(filename_a, filename_b):
-    """
-    returns differences between two files using get_opcodes
-    https://docs.python.org/3/library/difflib.html#difflib.SequenceMatcher.get_grouped_opcodes
-    """
-
-    with open(filename_a) as file_a:
-        a = file_a.read()
-
-    with open(filename_b) as file_b:
-        b = file_b.read()
-
-    results = []
-
-    s = difflib.SequenceMatcher(None, a, b)
-    for tag, i1, i2, j1, j2 in s.get_opcodes():
-        opcode = '{:7}   a[{}:{}] --> b[{}:{}] {!r:>8} --> {!r}'.format(tag, i1, i2, j1, j2, a[i1:i2], b[j1:j2])
-        results.append(opcode)
-
-    return results
-
 def sequence_matcher_opcodes_no_equal(filename_a, filename_b):
     """
     returns differences between two files using get_opcodes
--- main.py
+++ main.py
@@ -10,13 +10,5 @@ print('difflib_ndiff')
 pprint.pprint(difflib_ndiff('data/a.txt', 'data/b.txt'))
 
 print()
-print('difflib_unified_diff')
-pprint.pprint(difflib_unified_diff('data/a.txt', 'data/b.txt'))
-
-print()
-print('sequence_matcher_opcodes')
-pprint.pprint(sequence_matcher_opcodes('data/a.txt', 'data/b.txt'))
-
-print()
 print('sequence_matcher_opcodes_no_equal')
 pprint.pprint(sequence_matcher_opcodes_no_equal('data/b.txt', 'data/c.txt'))
