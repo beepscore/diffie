be3152efc193e57b1854b08100c6d7e7fd779e44
beepscore <support@beepscore.com>
2016-08-05 22:43:07 -0700

    In test_file_helper add test_files_in_dir

--- tests/test_file_helper.py
+++ tests/test_file_helper.py
@@ -41,5 +41,21 @@ class TestFileHelper(unittest.TestCase):
                     }
         self.assertEqual(expected, set(actual))
 
+    def test_files_in_dir(self):
+
+        ignored_filename_patterns = ['\A\.$', '\A\.\.$', '\A\.DS_Store$']
+        ignored_regex_objects = expression_helper.regex_objects_from_patterns(ignored_filename_patterns)
+
+        actual = file_helper.files_in_dir("./data/input/test",
+                                          ignored_regex_objects)
+
+        # Don't care about element order, so compare results using set instead of list
+        expected = {'a.txt',
+                    'b.txt',
+                    'c.txt',
+                    }
+
+        self.assertEqual(expected, set(actual))
+
 if __name__ == "__main__":
     unittest.main()
