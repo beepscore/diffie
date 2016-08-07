6a11cba8e23e023fcf283fc79e2469360004d7f1
beepscore <support@beepscore.com>
2016-08-07 14:53:34 -0700

    In main.py comment out some print statements

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import pprint
 # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 # if __name__ == '__main__':
 
-print('difflib_ndiff')
-#pprint.pprint(diffie.difflib_ndiff('data/input/a.txt', 'data/input/ac.txt'))
+# print('difflib_ndiff')
+# pprint.pprint(diffie.difflib_ndiff('data/input/test/a.txt', 'data/input/test/ac.txt'))
 
-print('pieces')
-# pieces = diffie.pieces('data/input/a.txt', 'data/input/ac.txt')
+# print('pieces')
+# pieces = diffie.pieces('data/input/test/a.txt', 'data/input/test/ac.txt')
 # for pair in pieces:
 #     print(pair[0])
 #     print(pair[1])
