4b039f6b2fbc0e9069fad5487a0ef4faad624ce8
beepscore <support@beepscore.com>
2016-08-07 12:48:18 -0700

    In main.py don't import *, prepend module name diffie before method namss.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-from diffie.diffie import *
+from diffie import diffie
 import pprint
 
 # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 # if __name__ == '__main__':
 
 print('difflib_ndiff')
-pprint.pprint(difflib_ndiff('data/input/a.txt', 'data/input/ac.txt'))
+pprint.pprint(diffie.difflib_ndiff('data/input/a.txt', 'data/input/ac.txt'))
 
 print('pieces')
-pieces = pieces('data/input/a.txt', 'data/input/ac.txt')
+pieces = diffie.pieces('data/input/a.txt', 'data/input/ac.txt')
 for pair in pieces:
     print(pair[0])
     print(pair[1])
     print()
 
-# get_pieces_and_write('data/input/a.txt', 'data/input/ac.txt', 'data/output', 'results.txt')
+# diffie.get_pieces_and_write('data/input/a.txt', 'data/input/ac.txt', 'data/output', 'results.txt')
 
-get_pieces_lines_and_write('data/input/a.txt', 'data/input/ac.txt', 'data/output', 'results.txt')
+diffie.get_pieces_lines_and_write('data/input/a.txt', 'data/input/ac.txt', 'data/output', 'results.txt')
